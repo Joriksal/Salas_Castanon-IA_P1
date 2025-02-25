dbc0d8e3013d916324dccf5bf97aa6b5d5af2a34
José Ricardo Salas Castañón <a22310269@ceti.mx>
2025-02-25 12:41:43 -0600

    Practica 3

---
+++ 003_Operadores.py
@@ -0,0 +1,41 @@
+"""Practica No. 3
+    Nombre: José Ricardo Salas Castañón
+    Registro: 22310269 
+    Grupo 6E
+    Materia: Inteligencia Artificial"""
+
+# 1 - Python como una calculadora
+
+# Operaciones básicas de suma, resta, multiplicación y división
+print(3 + 5)  # Suma
+print(10 - 2)  # Resta 
+print(4 * 7)  # Multiplicación
+print(20 / 5)  # División
+
+# 2 - Operadores Básicos
+
+# 3 - Exponenciación
+print(2 ** 3)  # 2 elevado a la potencia de 3
+
+# 4 - División Entera
+print(7 // 3)  # División entera de 7 entre 3
+
+# 5 - Módulo
+print(7 % 3)  # Resto de la división de 7 entre 3
+
+# 6 - Operadores Unarios
+
+print(+2) # Es dificil utilizarlo por su forma, ya que el operador conserva el argumento a su derecha y ya
+
+# 7 - Operadores y sus prioridades
+
+# Sin paréntesis (prioridad de operadores)
+# Primero se multiplica 5*2, luego se suma 3
+print(3 + 5 * 2)  
+
+# Con paréntesis (cambio en el orden de evaluación)
+# Primero se suma 3+5, luego se multiplica por 2
+print((3 + 5) * 2)  
+
+"""Sus prioridades tambien entra la jerarquia de operaciones y ya tambien se aplican
+    Asi tambien esto se aplica a los enteros, flotantes y booleanos"""
