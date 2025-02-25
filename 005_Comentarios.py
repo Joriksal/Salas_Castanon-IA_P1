8a6e54f5a43f4f5937b32f039da969f478f1156c
José Ricardo Salas Castañón <a22310269@ceti.mx>
2025-02-25 13:13:24 -0600

    Practica 5

---
+++ 005_Comentarios.py
@@ -0,0 +1,36 @@
+"""Practica No. 5
+    Nombre: José Ricardo Salas Castañón
+    Registro: 22310269 
+    Grupo 6E
+    Materia: Inteligencia Artificial"""
+
+""" 1 - La importancia de los comentarios: """
+
+# Este programa evalúa la hipotenusa c.
+# a y b son las longitudes de los cátetos.
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5  # Se emplea ** en lugar de una raíz cuadrada.
+print("c =", c)
+
+""" 2 - Marcar fragmentos para desabilitarlos: """
+
+# Este es un programa de prueba.
+x = 1
+y = 2
+# y = y + x
+print(x + y)
+
+""" 3 - A los comentarios se deben de hacer buen uso, no como este: """
+
+#este programa calcula los segundos en cierto número de horas determinadas
+# este programa fue escrito hace dos días
+
+a = 2 # número de horas
+seconds = 3600 # número de segundos en una hora
+
+print("Horas: ", a) #imprime el numero de horas
+# print("Segundos en Horas: ", a * seconds) # se imprime el numero de segundos en determinado numero de horas
+
+#aquí también se debe de imprimir un "Adiós", pero el programador no tuvo tiempo de escribirlo
+#este el es fin del programa que calcula el numero de segundos en 2 horas
